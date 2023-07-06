f4619cf1fbfc5d8b61a19e02c6a41a3fe82d8871
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-07-06 13:25:37 -0700

    copy from base example, and start to modify

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+    },
+    publish_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
---
+++ temp.js
@@ -0,0 +1,2 @@
+console.log(new Date().toJSON())
+console.log(new Date("2021-07-06").toJSON())
