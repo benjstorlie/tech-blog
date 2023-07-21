e8eb5dba7fddfcb10b11a0a088d1369bf7f63700
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-21 13:18:29 -0700

    new post page

--- public/js/commentForm.js
+++ public/js/commentForm.js
@@ -1,39 +1,40 @@
 
 
 const commentForm = document.getElementById('add-comment-form');
-const commentInput = document.querySelector('#add-comment-form textarea');
+const blogpostBodyEl = document.querySelector('#add-comment-form textarea');
 const submitButton = document.querySelector('.submit-btn');
-commentForm.addEventListener("submit", addComment);
+commentForm.addEventListener("submit", addPost);
 
 
-async function addComment(event) {
-  let commentBody = commentInput.value.trim();
+async function addPost(event) {
+  let commentBody = blogpostBodyEl.value.trim();
 
   if (!commentBody) {
-    commentInput.classList.add("is-invalid");
-    commentInput.addEventListener("change",() => {
-      commentInput.classList.remove("is-invalid");
+    blogpostBodyEl.classList.add("is-invalid");
+    blogpostBodyEl.addEventListener("change",() => {
+      blogpostBodyEl.classList.remove("is-invalid");
     }, {once: true})
     return;
   } else {
-    await saveComment(commentBody);
+    await savePost(commentBody);
   }
 }
 
-async function saveComment(commentBody) {
+async function savePost(commentBody) {
   submitButton.innerText = "Posting ..."
   const response = await fetch('/api/comment', {
     method: 'POST',
-    body: JSON.stringify(
-      commentBody
-    ),
+    body: JSON.stringify({
+      body: commentBody
+    }),
     headers: { 'Content-Type': 'application/json' },
   });
   if (response.ok) {
     submitButton.innerText = "Posted!!"
+    location.assign( location.pathname + '#comments');
   } else {
     submitButton.innerText = "Failed to Comment :(";
-    commentInput.addEventListener("change",() => {
+    blogpostBodyEl.addEventListener("change",() => {
       submitButton.innerText = "Post My Comment";
     }, {once: true})
   }
---
+++ public/js/newpost.js
@@ -0,0 +1,51 @@
+const newPostForm = document.getElementById('new-post-form');
+const blogpostTitleEl = document.querySelector('#new-post-form input');
+const blogpostBodyEl = document.querySelector('#new-post-form textarea');
+const submitButton = document.querySelector('.submit-btn');
+commentForm.addEventListener("submit", addPost);
+
+
+async function addPost(event) {
+  let blogpostBody = blogpostBodyEl.value.trim();
+  let blogpostTitle = blogpostTitleEl.value.trim();
+
+  if (!blogpostBody || !blogpostTitle) {
+    if (!blogpostTitle) {
+      blogpostTitleEl.classList.add("is-invalid");
+      blogpostTitleEl.addEventListener("change",() => {
+        blogpostTitleEl.classList.remove("is-invalid");
+      }, {once: true})
+    }
+    if (!blogpostBody) {
+      blogpostBodyEl.classList.add("is-invalid");
+      blogpostBodyEl.addEventListener("change",() => {
+        blogpostBodyEl.classList.remove("is-invalid");
+      }, {once: true})
+    }
+    return;
+
+  } else {
+    await savePost(blogpostTitle,blogpostBody);
+  }
+}
+
+async function savePost(title,body) {
+  submitButton.innerText = "Posting ..."
+  const response = await fetch('/api/blogpost', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    submitButton.innerText = "Posted!!";
+    location.assign('/blogpost/'+response.id);
+  } else {
+    submitButton.innerText = "Failed to Post :(";
+    blogpostBodyEl.addEventListener("change",() => {
+      submitButton.innerText = "Submit";
+    }, {once: true})
+  }
+}
