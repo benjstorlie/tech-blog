bd04d26d4e762cdf957417b7062e51249ae79999
bjs <127002178+benstorlie@users.noreply.github.com>
2023-07-20 18:43:52 -0700

    dark mode

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,6 +2,17 @@ const Handlebars = require("handlebars");
 
 const colors = ["primary","success","danger","warning","info"];
 
+// --blue: #007bff;
+//     --indigo: #6610f2;
+//     --purple: #6f42c1;
+//     --pink: #e83e8c;
+//     --red: #dc3545;
+//     --orange: #fd7e14;
+//     --yellow: #ffc107;
+//     --green: #28a745;
+//     --teal: #20c997;
+//     --cyan: #17a2b8;
+
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
